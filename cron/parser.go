b4705b04fe57ca1d10f61a68fc4424910f8e13a0
Chris Sun <62455458+Fzgt@users.noreply.github.com>
2023-01-25 21:55:08 +1100

    feat(cron): parse standard and seconds specs

---
+++ cron/parser.go
@@ -0,0 +1,69 @@
+package cron
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+// Parse parses a cron specification and returns a Schedule evaluated in UTC.
+// It accepts the standard five-field form (minute hour dom month dow) and the
+// six-field form with a leading seconds field.
+func Parse(spec string) (Schedule, error) {
+	return ParseInLocation(spec, time.UTC)
+}
+
+// ParseInLocation is like Parse but evaluates the schedule in loc.
+func ParseInLocation(spec string, loc *time.Location) (Schedule, error) {
+	spec = strings.TrimSpace(spec)
+	if spec == "" {
+		return nil, fmt.Errorf("cron: empty spec")
+	}
+	return parseFields(strings.Fields(spec), loc)
+}
+
+// parseFields builds a SpecSchedule from an already-split field list.
+func parseFields(fields []string, loc *time.Location) (*SpecSchedule, error) {
+	var sec, min, hour, dom, month, dow string
+	switch len(fields) {
+	case 5:
+		sec = "0"
+		min, hour, dom, month, dow = fields[0], fields[1], fields[2], fields[3], fields[4]
+	case 6:
+		sec, min, hour, dom, month, dow = fields[0], fields[1], fields[2], fields[3], fields[4], fields[5]
+	default:
+		return nil, fmt.Errorf("cron: expected 5 or 6 fields, got %d in %q", len(fields), strings.Join(fields, " "))
+	}
+
+	s := &SpecSchedule{Location: loc}
+	var err error
+	if s.Second, err = parseField(sec, secondsBound); err != nil {
+		return nil, err
+	}
+	if s.Minute, err = parseField(min, minutesBound); err != nil {
+		return nil, err
+	}
+	if s.Hour, err = parseField(hour, hoursBound); err != nil {
+		return nil, err
+	}
+	if s.Dom, err = parseField(dom, domBound); err != nil {
+		return nil, err
+	}
+	if s.Month, err = parseField(month, monthBound); err != nil {
+		return nil, err
+	}
+	if s.Dow, err = parseField(dow, dowBound); err != nil {
+		return nil, err
+	}
+	s.Dow = normalizeDow(s.Dow)
+	return s, nil
+}
+
+// normalizeDow folds the "Sunday is 7" convention onto "Sunday is 0" so both
+// spellings behave identically.
+func normalizeDow(mask uint64) uint64 {
+	if mask&(1<<7) != 0 {
+		mask = (mask &^ (1 << 7)) | 1
+	}
+	return mask
+}
