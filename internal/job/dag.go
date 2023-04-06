18ff3d49cd761e7655cbf866fa5e59368934e333
Chris Sun <62455458+Fzgt@users.noreply.github.com>
2023-04-06 15:11:26 +1000

    feat(job): add topological ordering and cycle check

--- internal/job/dag.go
+++ internal/job/dag.go
@@ -1,5 +1,15 @@
 package job
 
+import (
+	"errors"
+	"fmt"
+	"sort"
+)
+
+// ErrCycle is returned when a dependency graph cannot be ordered because it
+// contains a cycle.
+var ErrCycle = errors.New("job: dependency cycle detected")
+
 // Graph is a directed dependency graph over job IDs. An edge from A to B means
 // "B depends on A": A must finish before B may run. It is the structure the
 // scheduler walks to order a DAG of jobs.
@@ -54,4 +64,64 @@ func keys(m map[string]bool) []string {
 		out = append(out, k)
 	}
 	return out
+}
+
+// TopoSort returns the job IDs in an order where every job appears after all of
+// its dependencies. Ties are broken by ID so the ordering is deterministic. It
+// returns ErrCycle if the graph cannot be linearised.
+func (g *Graph) TopoSort() ([]string, error) {
+	indeg := make(map[string]int, len(g.nodes))
+	for id := range g.nodes {
+		indeg[id] = len(g.parents[id])
+	}
+
+	queue := make([]string, 0)
+	for id, d := range indeg {
+		if d == 0 {
+			queue = append(queue, id)
+		}
+	}
+
+	order := make([]string, 0, len(g.nodes))
+	for len(queue) > 0 {
+		sort.Strings(queue)
+		n := queue[0]
+		queue = queue[1:]
+		order = append(order, n)
+		for child := range g.children[n] {
+			indeg[child]--
+			if indeg[child] == 0 {
+				queue = append(queue, child)
+			}
+		}
+	}
+
+	if len(order) != len(g.nodes) {
+		return nil, ErrCycle
+	}
+	return order, nil
+}
+
+// BuildGraph constructs a Graph from a set of jobs, adding an edge from each
+// dependency to the job that declares it. It errors if a job depends on an
+// unknown ID or if the resulting graph contains a cycle.
+func BuildGraph(jobs []Job) (*Graph, error) {
+	g := NewGraph()
+	known := make(map[string]bool, len(jobs))
+	for _, j := range jobs {
+		g.AddNode(j.ID)
+		known[j.ID] = true
+	}
+	for _, j := range jobs {
+		for _, dep := range j.DependsOn {
+			if !known[dep] {
+				return nil, fmt.Errorf("job %q depends on unknown job %q", j.ID, dep)
+			}
+			g.AddEdge(dep, j.ID)
+		}
+	}
+	if _, err := g.TopoSort(); err != nil {
+		return nil, err
+	}
+	return g, nil
 }
