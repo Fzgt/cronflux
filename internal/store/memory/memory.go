bf24c3cebfc3ef4008519b0f2002db8d5146c2e3
Chris Sun <62455458+Fzgt@users.noreply.github.com>
2023-04-11 14:16:13 +1000

    feat(store): add run persistence and lease claim

--- internal/store/memory/memory.go
+++ internal/store/memory/memory.go
@@ -7,10 +7,14 @@ import (
 	"context"
 	"sort"
 	"sync"
+	"time"
 
 	"github.com/Fzgt/cronflux/internal/job"
 	"github.com/Fzgt/cronflux/internal/store"
 )
+
+// Store implements store.Store entirely in memory.
+var _ store.Store = (*Store)(nil)
 
 // Store keeps jobs and runs in memory behind a single mutex.
 type Store struct {
@@ -71,3 +75,111 @@ func (s *Store) DeleteJob(_ context.Context, id string) error {
 
 // Close is a no-op for the in-memory store.
 func (s *Store) Close() error { return nil }
+
+// CreateRun stores a new run.
+func (s *Store) CreateRun(_ context.Context, r job.Run) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.runs[r.ID] = r.Clone()
+	return nil
+}
+
+// GetRun returns a run by ID.
+func (s *Store) GetRun(_ context.Context, id string) (job.Run, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	r, ok := s.runs[id]
+	if !ok {
+		return job.Run{}, store.ErrNotFound
+	}
+	return r.Clone(), nil
+}
+
+// UpdateRun persists changes to an existing run.
+func (s *Store) UpdateRun(_ context.Context, r job.Run) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, ok := s.runs[r.ID]; !ok {
+		return store.ErrNotFound
+	}
+	s.runs[r.ID] = r.Clone()
+	return nil
+}
+
+// ListRuns returns runs matching the filter, newest first.
+func (s *Store) ListRuns(_ context.Context, f store.RunFilter) ([]job.Run, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	out := make([]job.Run, 0, len(s.runs))
+	for _, r := range s.runs {
+		if f.JobID != "" && r.JobID != f.JobID {
+			continue
+		}
+		if f.BatchID != "" && r.BatchID != f.BatchID {
+			continue
+		}
+		if f.State != "" && r.State != f.State {
+			continue
+		}
+		out = append(out, r.Clone())
+	}
+	sort.Slice(out, func(i, k int) bool {
+		if !out[i].CreatedAt.Equal(out[k].CreatedAt) {
+			return out[i].CreatedAt.After(out[k].CreatedAt)
+		}
+		return out[i].ID > out[k].ID
+	})
+
+	if f.Offset > 0 {
+		if f.Offset >= len(out) {
+			return []job.Run{}, nil
+		}
+		out = out[f.Offset:]
+	}
+	if f.Limit > 0 && len(out) > f.Limit {
+		out = out[:f.Limit]
+	}
+	return out, nil
+}
+
+// ClaimDue leases up to limit ready runs: pending runs that are due, plus
+// running runs whose lease has expired (the at-least-once redelivery path).
+func (s *Store) ClaimDue(_ context.Context, now time.Time, worker string, lease time.Duration, limit int) ([]job.Run, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	var ready []job.Run
+	for _, r := range s.runs {
+		switch {
+		case r.State == job.StatePending && !r.ScheduledFor.After(now):
+			ready = append(ready, r)
+		case r.State == job.StateRunning && r.LeaseExpiry.Before(now):
+			ready = append(ready, r)
+		}
+	}
+	sort.Slice(ready, func(i, k int) bool {
+		if !ready[i].ScheduledFor.Equal(ready[k].ScheduledFor) {
+			return ready[i].ScheduledFor.Before(ready[k].ScheduledFor)
+		}
+		return ready[i].ID < ready[k].ID
+	})
+	if limit > 0 && len(ready) > limit {
+		ready = ready[:limit]
+	}
+
+	claimed := make([]job.Run, 0, len(ready))
+	for _, r := range ready {
+		r.State = job.StateRunning
+		r.Worker = worker
+		r.LeaseExpiry = now.Add(lease)
+		if r.StartedAt == nil {
+			started := now
+			r.StartedAt = &started
+		}
+		r.UpdatedAt = now
+		s.runs[r.ID] = r
+		claimed = append(claimed, r.Clone())
+	}
+	return claimed, nil
+}
