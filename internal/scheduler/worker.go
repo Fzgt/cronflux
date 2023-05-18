1e61a908f781ba037352535b78fcac5047e720ea
Chris Sun <62455458+Fzgt@users.noreply.github.com>
2023-05-18 13:17:25 +1000

    feat(scheduler): handle retries and dead letters

--- internal/scheduler/worker.go
+++ internal/scheduler/worker.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	"time"
 
+	"github.com/Fzgt/cronflux/backoff"
 	"github.com/Fzgt/cronflux/internal/job"
 	"github.com/Fzgt/cronflux/internal/metrics"
 	"github.com/Fzgt/cronflux/internal/store"
@@ -50,9 +51,48 @@ func (w *worker) process(ctx context.Context, r job.Run) (job.Run, error) {
 		return r, w.store.UpdateRun(ctx, r)
 	}
 
-	// Retry and dead-letter handling is layered on separately.
-	r.State = job.StateFailed
+	// The execution failed. Retry with backoff until MaxRetries is spent, then
+	// move the run to the dead-letter state.
 	r.Error = execErr.Error()
-	w.metrics.ObserveRun(j.ID, r.State, dur)
+	if r.Attempt < j.MaxRetries {
+		r.State = job.StateFailed
+		if err := w.store.UpdateRun(ctx, r); err != nil {
+			return r, err
+		}
+		if err := w.scheduleRetry(ctx, j, r, finish); err != nil {
+			return r, err
+		}
+		w.metrics.ObserveRun(j.ID, job.StateFailed, dur)
+		w.metrics.IncRetry(j.ID)
+		return r, nil
+	}
+
+	r.State = job.StateDead
+	w.metrics.ObserveRun(j.ID, job.StateDead, dur)
 	return r, w.store.UpdateRun(ctx, r)
+}
+
+// scheduleRetry enqueues a fresh pending run for the next attempt, delayed by
+// the job's backoff policy.
+func (w *worker) scheduleRetry(ctx context.Context, j job.Job, failed job.Run, at time.Time) error {
+	delay := backoffFor(j.Backoff).Delay(failed.Attempt)
+	retry := job.Run{
+		ID:           w.newID(),
+		JobID:        failed.JobID,
+		BatchID:      failed.BatchID,
+		State:        job.StatePending,
+		Attempt:      failed.Attempt + 1,
+		ScheduledFor: at.Add(delay),
+		CreatedAt:    at,
+		UpdatedAt:    at,
+	}
+	if w.log != nil {
+		w.log.Debug("scheduling retry", "job", j.ID, "attempt", retry.Attempt, "delay", delay)
+	}
+	return w.store.CreateRun(ctx, retry)
+}
+
+// backoffFor builds an exponential backoff from a job's retry policy.
+func backoffFor(p job.RetryPolicy) backoff.Exponential {
+	return backoff.Exponential{Base: p.Base, Max: p.Max, Factor: p.Factor, Jitter: p.Jitter}
 }
